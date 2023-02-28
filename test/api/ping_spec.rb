6a2c4b1c3f2b6c78e3a5fe5e33ed1e803cfb66c2
Jérémie Veillet <jeremie.veillet@gmail.com>
2023-02-28 14:10:55 +0100

    Fixed tests in ping_spec

--- test/api/ping_spec.rb
+++ test/api/ping_spec.rb
@@ -7,30 +7,35 @@ describe PingController do
 
   it 'respond with a 200 status' do
     get '/api/v1/ping'
+
     assert_predicate(last_response, :ok?)
   end
 
   it 'respond with a status' do
     get '/api/v1/ping'
     response = JSON.parse(last_response.body)
+
     assert(response.key?('status'))
   end
 
   it 'has a status 200 in the body' do
     get '/api/v1/ping'
     response = JSON.parse(last_response.body)
+
     assert_equal(200, response['status'])
   end
 
   it 'respond with a message' do
     get '/api/v1/ping'
     response = JSON.parse(last_response.body)
+
     assert(response.key?('message'))
   end
 
   it 'respond with a Pong message' do
     get '/api/v1/ping'
     response = JSON.parse(last_response.body)
+
     assert_equal('Pong', response['message'])
   end
 end
